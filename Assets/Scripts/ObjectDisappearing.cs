81c453fb3ac6e6315b30ef3ff09b993bb867a15f
MaxLevchuk <101927674+MaxLevchuk@users.noreply.github.com>
2024-08-18 16:27:55 +0300

    New lght Test

--- Assets/Scripts/ObjectDisappearing.cs
+++ Assets/Scripts/ObjectDisappearing.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using UnityEngine.Rendering.Universal;
+
 public class ObjectDisappearing : MonoBehaviour
 {
     public float disappearTime = 4f;
 
-   private Light2D objLight;
+    private Light2D objLight;
     private SpriteRenderer spriteRenderer;
 
     private float creationTime;
@@ -14,22 +15,29 @@ public class ObjectDisappearing : MonoBehaviour
         objLight = GetComponent<Light2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
 
-     
         creationTime = Time.time;
     }
 
     void Update()
     {
-      
         float elapsedTime = Time.time - creationTime;
 
-       
         float currentAlpha = Mathf.Lerp(1f, 0f, elapsedTime / disappearTime);
 
         Color newColor = spriteRenderer.color;
         newColor.a = currentAlpha;
         spriteRenderer.color = newColor;
-        objLight.color = newColor;
+
+      
+        if (objLight != null)
+        {
+            objLight.color = newColor;
+        }
+        else 
+        {
+            Debug.Log("Light not set: " + gameObject.name.ToString());
+         }
+
         if (elapsedTime >= disappearTime)
         {
             Destroy(gameObject);
