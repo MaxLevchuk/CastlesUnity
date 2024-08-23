a560e0c6ac8db7237bb14fead078b5185d24f224
MaxLevchuk <101927674+MaxLevchuk@users.noreply.github.com>
2024-08-23 17:05:43 +0300

    Added Portal Patricles.

---
+++ Assets/Scripts/GravitationObj.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitationObj : MonoBehaviour
+{
+    public float gravityStrength = 9.8f; 
+    public float gravityRadius = 5.0f;   
+    public LayerMask affectedLayers;     
+
+    void FixedUpdate()
+    {
+       
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, gravityRadius, affectedLayers);
+
+        foreach (Collider2D collider in colliders)
+        {
+            Rigidbody2D rb = collider.attachedRigidbody;
+            if (rb != null)
+            {
+               
+                Vector2 direction = (transform.position - rb.transform.position).normalized;
+
+              
+                rb.AddForce(direction * gravityStrength * rb.mass);
+            }
+        }
+    }
+
+  
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, gravityRadius);
+    }
+}
--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
+    // Логіка повороту патріклсів вираховується від залежності руху снаряду у напрямку порталу
     public Transform connectedPortal;
     public float teleportCooldown = 0.5f;
-
+    public GameObject entryParticlePrefab;
+    public GameObject exitParticlePrefab;
     private bool canTeleport = true;
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -15,24 +17,17 @@ public class Portal : MonoBehaviour
             StartCoroutine(Teleport(other));
         }
     }
-
     private IEnumerator Teleport(Collider2D ball)
     {
         canTeleport = false;
-
         Portal connectedPortalScript = connectedPortal.GetComponent<Portal>();
         connectedPortalScript.canTeleport = false;
-
         Rigidbody2D rb = ball.GetComponent<Rigidbody2D>();
         Vector2 velocity = rb.velocity;
         float angularVelocity = rb.angularVelocity;
-
         Vector3 relativePosition = ball.transform.position - transform.position;
-
         Vector2 localVelocity = transform.InverseTransformDirection(velocity);
-
         GameObject newBall = Instantiate(ball.gameObject, connectedPortal.position + relativePosition, ball.transform.rotation);
-
         Rigidbody2D newRb = newBall.GetComponent<Rigidbody2D>();
         Vector2 portalVelocity = connectedPortal.TransformDirection(localVelocity);
         newRb.velocity = portalVelocity;
@@ -40,24 +35,76 @@ public class Portal : MonoBehaviour
 
         DisableAllComponents(ball);
 
+        GameObject exitParticles = Instantiate(exitParticlePrefab, newBall.transform.position, Quaternion.identity);
+        ParticleSystem exitParticleSystem = exitParticles.GetComponent<ParticleSystem>();
+        if (exitParticleSystem != null)
+        {
+            exitParticleSystem.Play();
+        }
+
+        GameObject entryParticles = Instantiate(entryParticlePrefab, ball.transform.position, Quaternion.identity);
+        ParticleSystem entryParticleSystem = entryParticles.GetComponent<ParticleSystem>();
+        if (entryParticleSystem != null)
+        {
+            entryParticleSystem.Play();
+        }
+
+        StartCoroutine(FollowEntryBall(entryParticles, newBall.transform));
+        StartCoroutine(FollowOutBall(exitParticles, newBall.transform));
+
         yield return new WaitForSeconds(teleportCooldown);
 
         canTeleport = true;
         connectedPortalScript.canTeleport = true;
 
-
         Destroy(ball.gameObject, 1f);
     }
+
+    private IEnumerator FollowOutBall(GameObject particles, Transform target)
+    {
+        ParticleSystem ps = particles.GetComponent<ParticleSystem>();
+        Rigidbody2D rb = target.GetComponent<Rigidbody2D>();
+        Vector2 localVelocity = transform.InverseTransformDirection(rb.velocity);
+        float angleOffset = 0f;
+        if (localVelocity.x > 0)
+        {
+            angleOffset = -90f;
+        }
+        else if (localVelocity.x < 0)
+        {
+            angleOffset = 90f;
+        }
+        float portalRotation = transform.eulerAngles.z;
+        particles.transform.rotation = Quaternion.Euler(0, 0, portalRotation + angleOffset);
+        yield break;
+    }
+
+    private IEnumerator FollowEntryBall(GameObject particles, Transform target)
+    {
+        ParticleSystem ps = particles.GetComponent<ParticleSystem>();
+        Rigidbody2D rb = target.GetComponent<Rigidbody2D>();
+        Vector2 localVelocity = transform.InverseTransformDirection(rb.velocity);
+        float angleOffset = 0f;
+        if (localVelocity.x > 0)
+        {
+            angleOffset = 90f;
+        }
+        else if (localVelocity.x < 0)
+        {
+            angleOffset = -90f;
+        }
+        float portalRotation = transform.eulerAngles.z;
+        particles.transform.rotation = Quaternion.Euler(0, 0, portalRotation + angleOffset);
+        yield break;
+    }
+
 
     private void DisableAllComponents(Collider2D ball)
     {
         foreach (var component in ball.GetComponents<MonoBehaviour>())
         {
-
             component.enabled = false;
-
         }
-
         ball.GetComponent<Collider2D>().enabled = false;
         ball.GetComponent<Rigidbody2D>().simulated = false;
         ball.GetComponent<SpriteRenderer>().enabled = false;
