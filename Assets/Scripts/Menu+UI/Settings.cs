d6473adf3957f0c32cc01aa6dc4ff2c04866e5e3
MaxLevchuk <101927674+MaxLevchuk@users.noreply.github.com>
2024-09-14 00:14:28 +0300

    Visual improvements to SnowGenerator

--- Assets/Scripts/Menu+UI/Settings.cs
+++ Assets/Scripts/Menu+UI/Settings.cs
@@ -1,13 +1,29 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class Settings : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    private ColorAdjustments colorAdjustments;
+    public Volume GlobalVolume;
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("General Volume", volume);
+    }
+
+    public void SetBrightness(float brightness)
+    {
+        if (GlobalVolume.profile.TryGet<ColorAdjustments>(out colorAdjustments))
+        {
+            colorAdjustments.postExposure.value = brightness;
+        }
+        else
+        {
+            Debug.LogWarning("ColorAdjustments is empty.");
+        }
     }
 }
